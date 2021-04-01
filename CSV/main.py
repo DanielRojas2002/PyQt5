6a06277349580f52fd5df21aaf78988a745185b8
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-03-31 18:25:32 -0600

    cambio

--- CSV/main.py
+++ CSV/main.py
@@ -11,6 +11,8 @@ notas=""
 titulo=""
 etiqueta1=""
 etiqueta2=""
+etiqueta3=""
+etiqueta4=""
 listaEncabezados=[]
 listadatos=[]
 class VentanaP1(QMainWindow):
@@ -22,8 +24,16 @@ class VentanaP1(QMainWindow):
         self.ui.realizar.clicked.connect(self.REALIZAR)
         self.ui.GRAFICAR.clicked.connect(self.Graficar)
 
+        self.ui.titulo_Grafico.setDisabled(True)
+        self.ui.etiqueta_nombre.setDisabled(True)
+        self.ui.etiqueta_valor.setDisabled(True)
+        self.ui.etiqueta_usuario.setDisabled(True)
+        self.ui.etiqueta_tiempo.setDisabled(True)
+        self.ui.GRAFICAR.setDisabled(True)
+
 
     def Buscar(self):
+        self.ui.GRAFICAR.setDisabled(True)
         try:
             global excel
             global notas
@@ -45,11 +55,21 @@ class VentanaP1(QMainWindow):
         global listadatos
         contador=0
         if self.ui.verCSV.isChecked()==True:
+            self.ui.titulo_Grafico.setDisabled(True)
+            self.ui.etiqueta_nombre.setDisabled(True)
+            self.ui.etiqueta_valor.setDisabled(True)
+            self.ui.etiqueta_usuario.setDisabled(True)
+            self.ui.etiqueta_tiempo.setDisabled(True)
+
+            self.ui.GRAFICAR.setDisabled(True)
             try:
                 self.ui.tituloGrafica.setText("")
                 self.ui.titulo_Grafico.setPlaceholderText("")
                 self.ui.etiqueta_nombre.setPlaceholderText("")
                 self.ui.etiqueta_valor.setPlaceholderText("")
+                self.ui.etiqueta_usuario.setPlaceholderText("")
+                self.ui.etiqueta_tiempo.setPlaceholderText("")
+
                 self.ui.encabezados.clear()
                 self.ui.tableWidget.clear()
                 listaEncabezados=[]
@@ -95,35 +115,87 @@ class VentanaP1(QMainWindow):
                 self.ui.errorarchivo.setText("El Archivo no se puede leer")
 
         elif self.ui.graficaPastel.isChecked()==True:
+            self.ui.titulo_Grafico.setEnabled(True)
+            self.ui.etiqueta_nombre.setEnabled(True)
+            self.ui.etiqueta_valor.setEnabled(True)
+            self.ui.GRAFICAR.setEnabled(True)
+
+
+            
+            self.ui.etiqueta_usuario.setDisabled(True)
+            self.ui.etiqueta_tiempo.setDisabled(True)
             global titulo
             global etiqueta1
             global etiqueta2
+            global etiqueta3
+            global etiqueta4
             titulo=""
             etiqueta1=""
             etiqueta2=""
+            etiqueta3=""
+            etiqueta4=""
             self.ui.tituloGrafica.setText("DATOS PARA PODER GRAFICAR:\nPASTEL:")
             self.ui.titulo_Grafico.setPlaceholderText("Ingrese el Titulo del Grafico:")
             self.ui.etiqueta_nombre.setPlaceholderText("Ingrese la Etiqueta del Nombre:")
             self.ui.etiqueta_valor.setPlaceholderText("Ingrese la Etiqueta del Valor:")
+            self.ui.etiqueta_usuario.setPlaceholderText("")
+            self.ui.etiqueta_tiempo.setPlaceholderText("")
             
             titulo=self.ui.titulo_Grafico.text()
             etiqueta1=self.ui.etiqueta_nombre.text()
             etiqueta2=self.ui.etiqueta_valor.text()
 
         elif self.ui.graficaBarras.isChecked()==True:
+            self.ui.titulo_Grafico.setEnabled(True)
+            self.ui.etiqueta_nombre.setEnabled(True)
+            self.ui.etiqueta_valor.setEnabled(True)
+            self.ui.GRAFICAR.setEnabled(True)
+
+            self.ui.etiqueta_usuario.setDisabled(True)
+            self.ui.etiqueta_tiempo.setDisabled(True)
+
             titulo=""
             etiqueta1=""
             etiqueta2=""
+            etiqueta3=""
+            etiqueta4=""
             self.ui.tituloGrafica.setText("DATOS PARA PODER GRAFICAR:\nBARRAS:")
             self.ui.titulo_Grafico.setPlaceholderText("Ingrese el Titulo del Grafico:")
             self.ui.etiqueta_nombre.setPlaceholderText("Ingrese la Etiqueta del Nombre:")
             self.ui.etiqueta_valor.setPlaceholderText("Ingrese la Etiqueta del Valor:")
+            self.ui.etiqueta_usuario.setPlaceholderText("")
+            self.ui.etiqueta_tiempo.setPlaceholderText("")
             
             titulo=self.ui.titulo_Grafico.text()
             etiqueta1=self.ui.etiqueta_nombre.text()
             etiqueta2=self.ui.etiqueta_valor.text()
-            
 
+        elif self.ui.graficaTenInd.isChecked()==True:
+
+            self.ui.titulo_Grafico.setEnabled(True)
+            self.ui.etiqueta_nombre.setEnabled(True)
+            self.ui.etiqueta_valor.setEnabled(True)
+            self.ui.etiqueta_usuario.setEnabled(True)
+            self.ui.etiqueta_tiempo.setEnabled(True)
+            self.ui.GRAFICAR.setEnabled(True)
+
+            titulo=""
+            etiqueta1=""
+            etiqueta2=""
+            etiqueta3=""
+            etiqueta4=""
+            self.ui.tituloGrafica.setText("DATOS PARA PODER GRAFICAR:\nTendencia\n(Usuario):")
+            self.ui.titulo_Grafico.setPlaceholderText("Ingrese el Titulo del Grafico:")
+            self.ui.etiqueta_nombre.setPlaceholderText("Ingrese la Etiqueta del Nombre:")
+            self.ui.etiqueta_valor.setPlaceholderText("Ingrese la Etiqueta del Valor:")
+            self.ui.etiqueta_usuario.setPlaceholderText("Ingrese la Etiqueta del Usuario:")
+            self.ui.etiqueta_tiempo.setPlaceholderText("Ingrese la Etiqueta del Tiempo:")
+
+            titulo=self.ui.titulo_Grafico.text()
+            etiqueta1=self.ui.etiqueta_nombre.text()
+            etiqueta2=self.ui.etiqueta_valor.text()
+            etiqueta3=self.ui.etiqueta_usuario.text()
+            etiqueta4=self.ui.etiqueta_tiempo.text()
 
 
     def Graficar(self):
@@ -132,10 +204,14 @@ class VentanaP1(QMainWindow):
         global titulo
         global etiqueta1
         global etiqueta2
+        global etiqueta3
+        global etiqueta4
         
-        if len(titulo)>0 and len(etiqueta1)>0 and len(etiqueta2)>0:
-            if self.ui.graficaPastel.isChecked()==True:
+       
+        if self.ui.graficaPastel.isChecked()==True:
+            if len(titulo)>0 and len(etiqueta1)>0 and len(etiqueta2)>0:
                 try:
+                    self.ui.errorarchivo.setText("")
                     notas=pd.read_csv(excel)
                     nombre=notas[etiqueta1]
                     valor=notas[etiqueta2]
@@ -154,11 +230,17 @@ class VentanaP1(QMainWindow):
                     plt.rc('legend', fontsize=6)
                     plt.legend(leyenda,loc='best',bbox_to_anchor=(1.05, 1.0))
                     plt.show()
+                    
                 except:
                     self.ui.errorarchivo.setText("Ingreso mal Una Etiqueta")
+            else:
+                self.ui.errorarchivo.setText("Ingrese los datos necesarios")
+        
 
-            elif self.ui.graficaBarras.isChecked()==True:
+        elif self.ui.graficaBarras.isChecked()==True:
+            if len(titulo)>0 and len(etiqueta1)>0 and len(etiqueta2)>0:
                 try:
+                    self.ui.errorarchivo.setText("")
                     notas=pd.read_csv(excel)
                     nombre=notas[etiqueta1]
                     valor=notas[etiqueta2]
@@ -171,16 +253,65 @@ class VentanaP1(QMainWindow):
                     plt.bar(nombre,valor)
                     plt.grid(True)
                     plt.show()
+                    
                 except:
                     self.ui.errorarchivo.setText("Ingreso mal Una Etiqueta")
+            else:
+                self.ui.errorarchivo.setText("Ingrese los datos necesarios")
+        
+
+        
+        if self.ui.graficaTenInd.isChecked()==True:
+            if len(titulo)>0 and len(etiqueta1)>0 and len(etiqueta2)>0 and len(etiqueta3)>0 and len(etiqueta4)>0:
+                try:
+                    self.ui.errorarchivo.setText("")
+                    notas=pd.read_csv(excel)
+                    notas[etiqueta3]=pd.to_datetime(notas[etiqueta3])
+
+                    dato=notas[etiqueta1]==etiqueta4
+                    DATOS=notas[dato]
+
+                    valor=DATOS[etiqueta2]
+                    tiempo=DATOS[etiqueta3]
+
+                    
+                    plt.style.use('seaborn')
+                    plt.plot_date(tiempo,valor,linestyle="solid")
+                    plt.gcf().autofmt_xdate()
+                    #formato=mpl_dates.DateFormatter('%b, %d, %Y')
+                    #plt.gca().xaxis.set_major_formatter(formato)
+                    plt.title(titulo+": "+etiqueta1+": "+etiqueta4)
+                    plt.xlabel(etiqueta3)
+                    plt.ylabel(etiqueta2)
+                    plt.tight_layout()
+                    plt.show()
+                    
+
+                except:
+                    self.ui.errorarchivo.setText("Ingreso mal Una Etiqueta")
+
+            else:
+                self.ui.errorarchivo.setText("Ingrese los datos necesarios")
+
+        
                 
+           
+        
 
-        else:
-            self.ui.errorarchivo.setText("Tiene que ingresar todos los Datos")
+            
 
         
 
         
+        
+            
+            
+
+            
+
+        
+
+
 if __name__=="__main__":
     app=QApplication(sys.argv)
     main=VentanaP1()
