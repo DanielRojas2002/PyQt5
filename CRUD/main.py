4849648659c50b6a34a18eaad17466e6db60f76c
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-04-19 20:39:57 -0500

    Completado la seccion CSV

--- CRUD/main.py
+++ CRUD/main.py
@@ -6,6 +6,7 @@ import time
 import webbrowser
 from tabulate import tabulate
 import datetime
+import pandas as pd
 
 from PyQt5.QtWidgets import QDialog,QApplication,QMainWindow,QMessageBox,QErrorMessage,QTableWidgetItem
 from codigos.creador import Ui_CREADORBD
@@ -26,6 +27,8 @@ from codigos.ventanacambiarregistrop1 import Ui_VentanacambiarRegistro
 from codigos.ventanamodificarregistro import Ui_VentanaModificarRegistros
 from codigos.ventanavtxt import Ui_VentanaValidacionTXT
 from codigos.ventanatxt import Ui_VentanaHacerTxt
+from codigos.ventanavcsv import Ui_VentanaValidacionCSV
+from codigos.ventanacsv import Ui_VentanaHacerCsv
 
 
 nombre=""
@@ -40,6 +43,7 @@ valorModificar=""
 primarykey=""
 campos=""
 seleccionTXT=""
+seleccionCSV=""
 contadorverificacion2=0
 contadorvalidacion2=0
 listaquery=[]
@@ -53,6 +57,7 @@ listacampos=[]
 diccInsertar={}
 listaDescripcion2=[]
 listaPrimarykey2=[]
+diccionariocsv={}
 contador=0
 contador2=1
 contadorRegistros=1
@@ -211,9 +216,295 @@ class Ventana2(QMainWindow):
             otraventana=VentanaVTxt(self)
             otraventana.show()
 
+        elif seleccion=="CSV":
+            self.hide()
+            otraventana=VentanaVCsv(self)
+            otraventana.show()
+
     def atras(self):
         self.parent().show()
         self.close()
+
+class VentanaVCsv(QMainWindow):
+    def __init__(self,parent=None):
+        super(VentanaVCsv,self).__init__(parent)
+        self.ui=Ui_VentanaValidacionCSV()
+        self.ui.setupUi(self)
+        self.ui.regresar.clicked.connect(self.atras)
+        self.ui.tablascreadas.itemClicked.connect(self.agregar)
+        self.ui.enviar.clicked.connect(self.ir)
+        
+       
+        global nombre
+        contador4=0
+        global listavalores
+        sql="SELECT name FROM sqlite_master WHERE type='table';"
+
+        try:
+            base=(f"CRUD/bases/{nombre}.db")
+            with sqlite3.connect(base) as conn:
+                c=conn.cursor()
+                c.execute(sql)
+                tablas=c.fetchall()
+
+                for t in tablas:
+                    for x in t:
+                        contador4=contador4+1
+                        self.ui.tablascreadas.addItem(x)
+                        listavalores.append(x)
+
+                if contador4==0:
+                    self.ui.tablascreadas.addItem("No existen Tablas tiene que crear tablas")
+
+        except:
+            self.ui.tablascreadas.addItem("No existen Tablas tiene que crear tablas")
+
+    	
+    def agregar(self):
+        dato=self.ui.tablascreadas.currentItem().text()
+        self.ui.nombretabla.setText(dato)
+
+    def ir(self):
+        global listavalores
+        global nombretabla
+        validacion=0
+        for x in listavalores:
+            if self.ui.nombretabla.text()==x:
+                validacion=validacion+1
+                nombretabla=self.ui.nombretabla.text()
+        
+        if validacion>0:
+            self.hide()
+            otraventana=VentanaCSV(self) #AQUI VA LA VENTA CSV
+            otraventana.show()
+        
+        else:
+            self.ui.error1.setText("No Existe esa Tabla")
+
+
+    def atras(self):
+        self.parent().show()
+        self.close()
+
+
+class VentanaCSV(QMainWindow):
+    def __init__(self,parent=None):
+        super(VentanaCSV,self).__init__(parent)
+        self.ui=Ui_VentanaHacerCsv()
+        self.ui.setupUi(self)
+        self.ui.regresar.clicked.connect(self.atras)
+        self.ui.elegir.clicked.connect(self.elegir)
+        self.ui.hacertcsv.clicked.connect(self.CSV)
+        self.ui.datoabuscar.setDisabled(True)
+        self.ui.hacertcsv.setDisabled(True)
+        self.ui.mensaje.setText("")
+        self.ui.nombrecampo.setText("")
+        
+
+        global nombre
+        global nombretabla
+        global listaDescripcion2
+        global seleccionCSV
+        global diccionariocsv
+        seleccionCSV=""
+        diccionariocsv={}
+
+
+        sql="PRAGMA table_info("
+        sql=sql+nombretabla+")"
+       
+
+        try:
+            listaDescripcion2=[]
+            base=(f"CRUD/bases/{nombre}.db")
+            with sqlite3.connect(base) as conn:
+                c=conn.cursor()
+                c.execute(sql)
+                tablas=c.fetchall()
+
+        
+                for a in tablas:
+                    for x in a[1:2]:
+                        listaDescripcion2.append(x)
+
+                for campo in listaDescripcion2:
+                    self.ui.combocampos.addItem(str(campo))
+
+                self.ui.combocampos.addItem("Todos")
+
+        except Error as e :
+            self.ui.mensaje.setText(e)
+        
+        
+
+
+    def elegir(self):
+        global seleccionCSV
+        seleccionCSV=self.ui.combocampos.itemText(self.ui.combocampos.currentIndex())
+
+        if len(seleccionCSV)>0:
+            self.ui.mensaje.setText("")
+            self.ui.datoabuscar.setEnabled(True)
+            self.ui.hacertcsv.setEnabled(True)
+            self.ui.nombrecampo.setText("Campo Seleccionado:"+seleccionCSV)
+            
+
+        else:
+            self.ui.nombrecampo.setText("Eliga un campo")
+            self.ui.mensaje.setText("Eliga una Opciones del Combobox")
+
+        if seleccionCSV=="Todos":
+            self.ui.datoabuscar.setDisabled(True)
+
+        else:
+            self.ui.datoabuscar.setEnabled(True)
+
+
+    def CSV(self):
+        global seleccionCSV
+        global listaDescripcion2
+        global nombretabla
+        global diccionariocsv
+        
+
+        if seleccionCSV=="Todos":
+            self.ui.datoabuscar.setDisabled(True)
+            contador=0
+            contador2=0
+            contadorregistro=0
+            lista=[]
+            
+            
+            sql="SELECT * FROM "+nombretabla
+
+            try:
+                base=(f"CRUD/bases/{nombre}.db")
+                with sqlite3.connect(base) as conn:
+                    c=conn.cursor()
+                    c.execute(sql)
+                    registros=c.fetchall()
+
+                
+                for elemento in registros:
+                    salto=len(elemento)
+         
+                    contador=contador+1 
+                    lista.append(registros[contadorregistro])
+                    contadorregistro=contadorregistro+1
+
+                         
+                if contador==0:
+                    listaregistro=[]
+                    contadorregistro=0
+                    self.ui.mensaje.setText("No se encontro registros")
+
+                else: 
+                    listafinal=[]
+                    for x in lista:
+                        for y in x:
+                            listafinal.append(y)
+                           
+                    listaFINAL=[]
+                    inicio=0
+                
+                    for x in range(1,salto+1):
+                        listaFINAL.append(list(listafinal[inicio::salto]))
+                        inicio=inicio+1
+
+                    
+                    for elemento in listaFINAL:
+                        diccionariocsv[listaDescripcion2[contador2]]=elemento
+                        contador2=contador2+1
+
+                    diccionarioCSV=pd.DataFrame(diccionariocsv)
+
+                    tiempo=datetime.datetime.now()
+                    TIEMPO=tiempo.strftime('_%M_%S')
+                    cadena=""
+                    cadena="CRUD/Reportes/Reporte_por_"+"TODOS_"+nombretabla+TIEMPO+".csv"
+                    
+                    diccionarioCSV.to_csv(cadena, index=None, mode="a", header=not os.path.isfile(cadena))
+                  
+                    
+                    self.ui.mensaje.setText("El Reporte CSV esta hecho")
+                    
+
+            except Error as e:
+                self.ui.mensaje.setText(e)
+
+            
+
+        else:
+            self.ui.datoabuscar.setEnabled(True)
+            contador=0
+            contador2=0
+            contadorregistro=0
+            lista=[]
+        
+            sql="SELECT * FROM "+nombretabla+" WHERE "+seleccionCSV+"= :"+seleccionCSV
+            dato=self.ui.datoabuscar.text()
+
+            if len(dato)>0:
+                self.ui.mensaje.setText("")
+
+                valor={seleccionCSV:dato}
+                try:
+                    base=(f"CRUD/bases/{nombre}.db")
+                    with sqlite3.connect(base) as conn:
+                        c=conn.cursor()
+                        c.execute(sql,valor)
+                        registros=c.fetchall()
+
+                
+                    for elemento in registros:
+                        salto=len(elemento)
+            
+                        contador=contador+1 
+                        lista.append(registros[contadorregistro])
+                        contadorregistro=contadorregistro+1
+                
+                    if contador==0:
+                        self.ui.mensaje.setText("No se encontro registros")
+
+                    else:
+                        listafinal=[]
+                        for x in lista:
+                            for y in x:
+                                listafinal.append(y)
+                            
+                        listaFINAL=[]
+                        inicio=0
+                    
+                        for x in range(1,salto+1):
+                            listaFINAL.append(list(listafinal[inicio::salto]))
+                            inicio=inicio+1
+
+                        
+                        for elemento in listaFINAL:
+                            diccionariocsv[listaDescripcion2[contador2]]=elemento
+                            contador2=contador2+1
+
+                        diccionarioCSV=pd.DataFrame(diccionariocsv)
+
+                        tiempo=datetime.datetime.now()
+                        TIEMPO=tiempo.strftime('_%M_%S')
+                        cadena=""
+                        cadena="CRUD/Reportes/Reporte_por_"+seleccionCSV+"_"+dato+"_"+nombretabla+TIEMPO+".csv"
+                        
+                        diccionarioCSV.to_csv(cadena, index=None, mode="a", header=not os.path.isfile(cadena))
+                        self.ui.mensaje.setText("El Reporte CSV esta hecho")
+                        
+                except Error as e:
+                    self.ui.mensaje.setText(e)
+
+            else:
+                self.ui.mensaje.setText("Ingrese el valor a buscar")
+                 
+
+    def atras(self):
+        self.parent().show()
+        self.close()
+
 
 
 class VentanaVTxt(QMainWindow):
@@ -353,6 +644,7 @@ class VentanaTXT(QMainWindow):
     def TXT(self):
         global seleccionTXT
         global listaDescripcion2
+        global nombretabla
 
         if seleccionTXT=="Todos":
             self.ui.datoabuscar.setDisabled(True)
@@ -379,10 +671,10 @@ class VentanaTXT(QMainWindow):
                     tiempo=datetime.datetime.now()
                     TIEMPO=tiempo.strftime('Dia:%d Mes:%m Año:%Y Hora:%H Minuto:%M Segundo:%S')
                     cadena=""
-                    cadena="CRUD/Reportes/Reporte_por_"+"TODOS"
+                    cadena="CRUD/Reportes/Reporte_por_"+"TODOS_"+nombretabla+".txt"
                     archivoA=open(cadena ,'a',encoding="utf-8")
                     archivoA.write("\n")
-                    archivoA.write("-"*20+"Buscado por : "+"Todos"+":"+" ("+TIEMPO+")"+"-"*20)
+                    archivoA.write("-"*20+"Buscado por : "+"Todos"+":"+" Tabla_"+nombretabla+" ( "+TIEMPO+")"+"-"*20)
                     archivoA.write("\n")
                     for conjunto in registros:
                         datos.append(conjunto)
@@ -429,10 +721,10 @@ class VentanaTXT(QMainWindow):
                         tiempo=datetime.datetime.now()
                         TIEMPO=tiempo.strftime('Dia:%d Mes:%m Año:%Y Hora:%H Minuto:%M Segundo:%S')
                         cadena=""
-                        cadena="CRUD/Reportes/Reporte_por_"+seleccionTXT+"_"+dato
+                        cadena="CRUD/Reportes/Reporte_por_"+seleccionTXT+"_"+dato+"_"+nombretabla+".txt"
                         archivoA=open(cadena ,'a',encoding="utf-8")
                         archivoA.write("\n")
-                        archivoA.write("-"*20+"Buscado por : "+seleccionTXT+":"+dato+" ("+TIEMPO+")"+"-"*20)
+                        archivoA.write("-"*20+"Buscado por : "+seleccionTXT+":"+dato+" Tabla_"+nombretabla+" ("+TIEMPO+")"+"-"*20)
                         archivoA.write("\n")
                         for conjunto in registros:
                             datos.append(conjunto)
